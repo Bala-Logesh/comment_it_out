9c664c58183b37f46a0b996096ad883345379ef4
Bala Logesh S <balassmp@gmail.com>
2021-08-01 19:58:33 +0530

    Finished the modal

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../css/Popup.css'
 
 const Popup = () => {
@@ -13,7 +14,11 @@ const Popup = () => {
     return (
         <div className={ modal ? 'Popup flex' : 'hide-popup' } id='popup' onClick={ closeModal }>
             <div className="Popup__container flex">
-                Happy popup
+                <h2>Are you sure to logout?</h2>
+                <div className="Popup__buttons flex">
+                    <button className='btn' type="submit">Logout</button>
+                    <Link to='/home' onClick={ () => setModal(false) }><button className='btn'>Cancel</button></Link>
+                </div>
             </div>
         </div>
     )
