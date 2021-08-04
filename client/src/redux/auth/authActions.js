dbed4c6b44e7d0c9edc089549546db09d87cc5ff
Bala Logesh S <balassmp@gmail.com>
2021-08-05 02:23:19 +0530

    Completed auth reducer

--- client/src/redux/auth/authActions.js
+++ client/src/redux/auth/authActions.js
@@ -1,12 +1,14 @@
 import { AUTH_LOGIN, AUTH_REGISTER, LOGOUT_USER, AUTH_TOKEN_LOGIN } from "./authTypes";
-import { loginError, registerError } from '../'
+import { loginError, registerError, setLoading } from '../'
 import { push } from 'connected-react-router'
 import * as API from '../../api'
 import actionHelper from "../utils/actionHelper";
 
 /////////////////////////////////////////////////////////////////////////// Login an existing user from token
 export const loginUsingToken = () => async (dispatch) => {
+    dispatch(setLoading())
     const token = JSON.parse(localStorage.getItem('token'))
+    console.log(token);
     
     const { data } = await API.auth(token)
         
@@ -16,6 +18,7 @@ export const loginUsingToken = () => async (dispatch) => {
 
 /////////////////////////////////////////////////////////////////////////// Login an existing User
 export const loginUser = (user) => async (dispatch) => { 
+    dispatch(setLoading())
     const { data } = await API.login(user)
         
     actionHelper(dispatch, data, AUTH_LOGIN, loginError)
@@ -24,12 +27,14 @@ export const loginUser = (user) => async (dispatch) => {
 
 /////////////////////////////////////////////////////////////////////////// Register a new User
 export const registerUser = (user) => async (dispatch) => {
+    dispatch(setLoading())
     const { data } = await API.register(user)
         
     actionHelper(dispatch, data, AUTH_REGISTER, registerError)
     dispatch(push('/home'))
 }
 
+/////////////////////////////////////////////////////////////////////////// Logout the current user
 export const logoutUser = () =>(dispatch) => {
     dispatch({
         type: LOGOUT_USER
